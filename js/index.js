fb87ce5c1d253791731c00d5904b2d994fe4c5da
Kirill <beerbox7@yandex.com>
2022-09-19 23:39:01 +0400

    dlk
    upd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 var popup = document.querySelector('.popup')
 var popapBtn = document.querySelector('.banner_js')
 var popapClose = document.querySelector('.popap__close')
+var check = document.querySelector ('#input__check')
+var submit = document.querySelector ('.button__submit')
+submit.setAttribute('disabled', true)
+
+check.oninput = function() {
+    if (check.checked) {
+        submit.removeAttribute('disabled');
+    }else{
+        submit.setAttribute('disabled', true);
+    }
+}
 
 popapBtn.addEventListener('click', function() {
     popup.classList.add('popup_open')
@@ -9,4 +20,3 @@ popapBtn.addEventListener('click', function() {
 popapClose.addEventListener('click', function() {
     popup.classList.remove('popup_open')
 })
-
